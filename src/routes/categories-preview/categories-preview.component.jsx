59e3d547c19d8f7c5f93b9474679644644020fef
Cremacious <108367916+Cremacious@users.noreply.github.com>
2024-12-02 16:55:27 -0500

    stuff

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -4,7 +4,7 @@ import { CategoriesContext } from '../../contexts/categories.context';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
 
   return (
     <Fragment>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
@@ -7,14 +8,38 @@ import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
 
+const GET_CATEGORIES = gql`
+  query($title: String!) {
+  getCollectionsByTitle(title: $title) {
+  id
+  title
+  items {
+    id
+    name
+    price
+    imageUrl}
+  }
+`;
+
 const Category = () => {
   const { category } = useParams();
+  const { loading, error, data } = useQuery(GET_CATEGORIES, {
+    variables: { title: category },
+  });
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+  // useEffect(() => {
+  //   setProducts(categoriesMap[category]);
+  // }, [category, categoriesMap]);
+
+useEffect(()=> {
+  if (data) {
+    const { getCollectionsByTitle: {items }} = data;
+    setProducts(items);
+  }
+}, [category, data])
+
 
   return (
     <Fragment>
